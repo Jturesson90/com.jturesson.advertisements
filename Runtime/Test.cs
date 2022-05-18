5155c7cb0121b87189a5f157e6e41f1f4f14cf12
Jesper Turesson <Jturesson90@gmail.com>
2022-05-18 16:38:34 +0200

    Added unityengine.advertisements

--- Runtime/Test.cs
+++ Runtime/Test.cs
@@ -1,5 +1,11 @@
+using System;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class Test : MonoBehaviour
 {
+    private void Awake()
+    {
+        var a = Advertisement.isShowing;
+    }
 }
